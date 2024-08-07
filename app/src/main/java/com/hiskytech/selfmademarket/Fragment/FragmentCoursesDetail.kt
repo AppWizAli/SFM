99e753dd20f14ea53cb6e997c9b9d6e1085a9215
AhmadDevLAb <devdroid47@gmail.com>
2024-08-07 12:34:36 -0700

    Api Completion

--- app/src/main/java/com/hiskytech/selfmademarket/Fragment/FragmentCoursesDetail.kt
+++ app/src/main/java/com/hiskytech/selfmademarket/Fragment/FragmentCoursesDetail.kt
@@ -7,13 +7,10 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.hiskytech.selfmademarket.Adapter.VideoAdapter
-import com.hiskytech.selfmademarket.Model.ModelVideo
+
 import com.hiskytech.selfmademarket.R
-import com.squareup.picasso.Picasso
 import android.widget.Button
 import android.widget.TextView
-import com.hiskytech.selfmademarket.Adapter.AdapterCourseDetails
 import de.hdodenhof.circleimageview.CircleImageView
 
 class FragmentCoursesDetail : Fragment() {
@@ -23,7 +20,7 @@ class FragmentCoursesDetail : Fragment() {
     private lateinit var courseInstructor: TextView
     private lateinit var courseDuration: TextView
     private lateinit var courseVideos: RecyclerView
-    private lateinit var videoList: List<ModelVideo>
+//    private lateinit var videoList: List<ModelVideo>
     private lateinit var beginnersButton: Button
     private lateinit var proButton: Button
 
@@ -39,9 +36,9 @@ class FragmentCoursesDetail : Fragment() {
         courseImage = view.findViewById(R.id.imgdetails) // Updated ID
         courseInstructor = view.findViewById(R.id.user_name) // Updated ID
         courseDuration = view.findViewById(R.id.hour) // Updated ID
-        courseVideos = view.findViewById(R.id.rvcourses)
-        beginnersButton = view.findViewById(R.id.beginner_button) // Updated ID
-        proButton = view.findViewById(R.id.pro_button) // Updated ID
+//        courseVideos = view.findViewById(R.id.rvcourses)
+//        beginnersButton = view.findViewById(R.id.beginner_button) // Updated ID
+//        proButton = view.findViewById(R.id.pro_button) // Updated ID
 
         // Retrieve data from arguments
         val args = arguments
@@ -49,13 +46,13 @@ class FragmentCoursesDetail : Fragment() {
             courseName.text = args.getString("COURSE_NAME")
             courseInstructor.text = args.getString("COURSE_INSTRUCTOR")
             courseDuration.text = args.getString("COURSE_DURATION")
-            videoList = args.getParcelableArrayList("COURSE_VIDEOS") ?: emptyList()
-            Picasso.get().load(args.getString("COURSE_IMAGE")).into(courseImage)
+//            videoList = args.getParcelableArrayList("COURSE_VIDEOS") ?: emptyList()
+//            Picasso.get().load(args.getString("COURSE_IMAGE")).into(courseImage)
         }
 
         // Set up RecyclerView
         courseVideos.layoutManager = LinearLayoutManager(context)
-        courseVideos.adapter = AdapterCourseDetails(videoList)
+//        courseVideos.adapter = AdapterCourseDetails(videoList)
 
         // Set up Button click listeners if needed
         beginnersButton.setOnClickListener {
